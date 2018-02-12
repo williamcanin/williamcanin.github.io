062476762732fcfd713d301de1c44a6439370ae4
William C. Canin <william.costa.canin@gmail.com>
2018-02-11 23:18:06 -0200

    Update - dom fev 11 23:18:05 -02 2018

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,4 +1,1 @@
-/* Cursor (pipe) flashing in the page Hello */
-setInterval(function() {
-  $(".layout_hello__cursor").toggle()
-},600);
+setInterval(function(){$(".layout_hello__cursor").toggle()},600);
