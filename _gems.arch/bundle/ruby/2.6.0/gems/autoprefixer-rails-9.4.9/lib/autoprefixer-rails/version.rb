055294dcdf849dc535fa784876ec8ae904ac4411
William C. Canin <william.costa.canin@gmail.com>
2019-05-09 21:47:23 -0300

    Update - qui mai  9 21:47:23 -03 2019

--- _gems.arch/bundle/ruby/2.6.0/gems/autoprefixer-rails-9.4.9/lib/autoprefixer-rails/version.rb
+++
@@ -1,3 +0,0 @@
-module AutoprefixerRails
-  VERSION = '9.4.9'.freeze unless defined? AutoprefixerRails::VERSION
-end
--- _gems.arch/bundle/ruby/2.6.0/gems/jektify-1.0.3/lib/jektify.rb
+++
@@ -1,6 +0,0 @@
-# Load partials
-
-require "jektify/version"
-require "jektify/engine"
-require "jektify/render"
-require "jektify/main"
