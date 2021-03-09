58011c3bcfd20b58a27021ad37593818fa551e5f
William C. Canin <william.costa.canin@gmail.com>
2021-03-09 20:04:14 -0300

    Update - D:09032021-H:200414

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,1 @@
-(function(){}).call(this);
+(function(){0===location.href.indexOf("https://williamcanin.me/contact/#email_successfully_sent")&&($("#email_successfully_sent").modal("show"),$("#email_successfully_sent").on("hidden.bs.modal",function(){return document.location.href=String(document.location.href).replace("#email_successfully_sent",""),!1}))}).call(this);
