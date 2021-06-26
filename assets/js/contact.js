cd74dbf7d95915109e38797d18ebfcd64c9583ef
William C. Canin <william.costa.canin@gmail.com>
2021-06-26 12:22:57 -0300

    Update - D:26062021-H:122257

--- assets/js/contact.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  $('#submit').click(function() {
-    $('#name').val('');
-    $('#subject').val('');
-    $('#email').val('');
-    $('#message').val('');
-  });
-
-}).call(this);
--- assets/js/hello.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  setInterval(function() {
-    return $(".layout_hello__cursor").toggle();
-  }, 600);
-
-}).call(this);
