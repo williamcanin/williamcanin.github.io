af03c8745f51968dad458b87c0eb7fabe3fb765a
William C. Canin <william.costa.canin@gmail.com>
2022-06-23 20:35:45 -0300

    update

---
+++ assets/js/post.js
@@ -0,0 +1,29 @@
+(function() {
+  $('#markdown-toc').hover(function() {
+    return $(this).attr('data-content', 'Índice');
+  });
+
+  $("#markdown-toc a").click(function() {
+    return $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 500);
+  });
+
+  $("#comments_in_post").click(function() {
+    var disqus_shortname;
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 500);
+    disqus_shortname = 'williamcanin';
+    return function() {
+      var s;
+      s = document.createElement('script');
+      s.async = true;
+      s.type = 'text/javascript';
+      s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+      return (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    };
+  });
+
+}).call(this);
