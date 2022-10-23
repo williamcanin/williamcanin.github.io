cb221431a7f20da4ed8d5424fb535d65098e317a
William C. Canin <william.costa.canin@gmail.com>
2022-10-23 11:12:30 -0300

    update

---
+++ assets/js/global.js
@@ -0,0 +1,16 @@
+(function() {
+  $('.scrolltop__button').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 'slow');
+    return false;
+  });
+
+
+  /*
+   * Welcome to the new js2coffee 2.0, now
+   * rewritten to use the esprima parser.
+   * try it out!
+   */
+
+}).call(this);
