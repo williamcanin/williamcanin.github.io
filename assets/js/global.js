94e5e5f7f63ffd0e3572830b335a1671d8e9d13c
William C. Canin <william.costa.canin@gmail.com>
2025-09-20 13:15:55 -0300

    Update

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,16 +1,1 @@
-(function() {
-  $('.scrolltop__button').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 'slow');
-    return false;
-  });
-
-
-  /*
-   * Welcome to the new js2coffee 2.0, now
-   * rewritten to use the esprima parser.
-   * try it out!
-   */
-
-}).call(this);
+!function(){$(".scrolltop__button").click(function(){return $("html, body").animate({scrollTop:0},"slow"),!1})}.call(this);
--- assets/js/postlist.js
+++ assets/js/postlist.js
@@ -1,15 +1,1 @@
-(function() {
-  var disqus_shortname;
-
-  disqus_shortname = 'williamcanin';
-
-  (function() {
-    var s;
-    s = document.createElement('script');
-    s.async = true;
-    s.type = 'text/javascript';
-    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-    return (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-  });
-
-}).call(this);
+!function(){}.call(this);
